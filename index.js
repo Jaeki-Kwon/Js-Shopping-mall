dc7254e5f49976d527d037622072e0e3c7d4e0cb
Jaeki-Kwon <worl1126@naver.com>
2021-01-18 10:55:48 +0900

    2021 01 18
    Update

---
+++ index.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector(".circle");
+const section = document.querySelector("section");
+const input = document.querySelector("input");
+
+const deleteMenu = (event) => {
+  const btn = event.target;
+  const menu = btn.parentNode;
+  //   console.log(event.target.parentNode);
+  section.removeChild(menu);
+};
+
+const click = () => {
+  const div = document.createElement("div");
+  const span = document.createElement("span");
+  const i = document.createElement("i");
+
+  div.setAttribute("class", "menu");
+  span.setAttribute("class", "menu-kind");
+  i.setAttribute("class", "fas fa-trash-alt");
+
+  span.textContent = input.value;
+
+  if (input.value === "") {
+    console.log("빈칸");
+  } else {
+    section.appendChild(div);
+    div.appendChild(span);
+    div.appendChild(i);
+  }
+
+  i.addEventListener("click", deleteMenu);
+  input.value = "";
+};
+
+const enter = (e) => {
+  if (e.key === "Enter") {
+    const div = document.createElement("div");
+    const span = document.createElement("span");
+    const i = document.createElement("i");
+
+    div.setAttribute("class", "menu");
+    span.setAttribute("class", "menu-kind");
+    i.setAttribute("class", "fas fa-trash-alt");
+
+    span.textContent = input.value;
+    if (input.value === "") {
+      console.log("빈칸");
+    } else {
+      section.appendChild(div);
+      div.appendChild(span);
+      div.appendChild(i);
+    }
+
+    i.addEventListener("click", deleteMenu);
+    input.value = "";
+  }
+};
+
+input.addEventListener("keypress", enter);
+btn.addEventListener("click", click);
